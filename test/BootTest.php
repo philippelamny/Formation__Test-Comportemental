42613d9c45b9640891119b7dff345db7639d046a
philippe lam <philippe.lam@tagpay.fr>
2022-03-20 12:04:04 +0100

    ok ok

---
+++ test/BootTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Trung\Ftc\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class BootTest extends TestCase
+{
+    public function test_BootConfig(): void
+    {
+        $this->assertTrue(true);
+    }
+}
